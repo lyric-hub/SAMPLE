0485a2d8cd1de61d2e0e3d8904a95d9afb9dd8ed
CYRIL SAJU <csaju53@gmail.com>
2025-07-09 16:13:11 +0530

    update

--- datasets.py
+++ datasets.py
@@ -1,5 +1,7 @@
-
 import json
-with open("patients.json") as f:
+import os
+BASE_DIR = os.path.dirname(__file__)
+json_path = os.path.join(BASE_DIR, "patients.json")
+with open(json_path) as f:
     patient_data = json.load(f)
 patients = [p["text"] for p in patient_data]
